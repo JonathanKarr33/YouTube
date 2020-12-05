5fff222087bb0f57f0df81514c39dc77e2c54b64
Jonathan Karr <jka83321@gmail.com>
2020-12-04 20:55:33 -0500

    rest of tutorial 4

---
+++ calories.py
@@ -0,0 +1,10 @@
+age = int(input("What is your age: "))
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+age = int(input("What is your age: "))
+calories_breakfast = float(input("How many calories did you eat for breakfast:"))
+calories_lunch = float(input("How many calories did you eat for lunch:"))
+calories_dinner = float(input("How many calories did you eat for dinner:"))
+calories = calories_breakfast + calories_lunch + calories_dinner
+print("Hello " + first_name.capitalize() + " " + last_name.upper() + " you are " + str(age))
+print("You ate " + str(calories) + " calories \n today")
