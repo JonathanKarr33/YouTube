8805fe7905be073061604ed71383e48810839cd8
Jonathan Karr <jka83321@gmail.com>
2020-12-08 23:49:34 -0500

    Tutorial 6

---
+++ Tutorial 6/number_types.py
@@ -0,0 +1,8 @@
+answer = 4 + 5.7
+print(answer)
+answer = 4 - 5.7
+print(round(answer,1))
+print(0o17)
+print(0x17)
+print(0x1a)
+print(7E6)
---
+++ Tutorial 6/operations.py
@@ -0,0 +1,8 @@
+print(4+7)
+print(4-7)
+print(4*7)
+print(4/7)
+print(4/4)
+print(5//4)
+print(5%4)
+print(3**3)
