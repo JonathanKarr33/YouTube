154867e081b0daff8573961172b8607f8b807ddc
Jonathan Karr <jka83321@gmail.com>
2020-12-10 23:13:37 -0500

    fixed 2nd print statement

--- Tutorial 3/comments_and_data_types.py
+++ Tutorial 3/comments_and_data_types.py
@@ -6,4 +6,4 @@ name = input("What is your name: ")
 age = int(input("What is your age: "))
 
 print("Hello " + name + ". You are " + str(age))
-print("Hello" , name + ". You are", age)
+print("Hello" , name , "you are", age)
