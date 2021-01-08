677e796b38be632dc83472b39b04d0ec81c6d409
Jonathan Karr <jka83321@gmail.com>
2021-01-08 16:58:50 -0500

    Tutorial 16

---
+++ Tutorial 16/tuples.py
@@ -0,0 +1,10 @@
+my_tuple_1 = (1,4,5)
+my_tuple_2 = 6,4,6,5,3
+print(my_tuple_1[1])
+print(my_tuple_2)
+print(len(my_tuple_2))
+print(10 in my_tuple_1)
+print(5 not in my_tuple_1)
+my_tuple_1.append(8) #This doesn't work since tuples are immutable
+cordinates = (47,32)
+red = (255,0,0)
