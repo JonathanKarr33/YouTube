19fc7cdee00e12fe678de2377142db6c642188fc
Jonathan Karr <jka83321@gmail.com>
2021-07-07 20:13:27 -0400

    Tutorial 54 - FizzBuzz

---
+++ Tutorial 54/fizzbuzz.py
@@ -0,0 +1,9 @@
+num = int(input("Enter an integer: "))
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print(num)
