448c3f7093881cf91f445d331e8d37aa735abf2e
JonathanKarr33 <jka83321@gmail.com>
2022-03-27 14:07:24 -0400

    while loops

--- C/tutorial_7/break_and_continue.c
+++ C/tutorial_7/break_and_continue.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-int main() {
+int main(){
     int i = 1;
-    
-    while (i <= 5) {
-        printf("%d\n", i);
+
+    while(i <= 5){
         if (i == 3){
-            break;
+            i++;
+            continue;
         }
+        printf("%d\n", i);
         i++;
     }
     printf("We are finished with the loop\n");
-  return 0;
+    return 0;
 }
--- C/tutorial_7/do_while.c
+++ C/tutorial_7/do_while.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int main() {
+int main(){
     int i = 1;
-    
+
     do {
         printf("%d\n", i);
         i++;
     }
-    while (i <=0);
+    while(i <= 0);
     printf("We are finished with the loop\n");
-  return 0;
+    return 0;
 }
