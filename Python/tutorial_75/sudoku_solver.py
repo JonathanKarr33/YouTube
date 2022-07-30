6ed46f2425ed1b7ac19675d2a79b9028534cffc0
JonathanKarr33 <jka83321@gmail.com>
2022-07-30 18:02:40 -0400

    sudoku solver

--- Python/tutorial_75/sudoku_solver.py
+++ Python/tutorial_75/sudoku_solver.py
@@ -11,16 +11,15 @@ board = [
     [0,1,0,3,8,9,0,0,0]
 ]
 
-
 def solve(board : list):
     find = next_empty_space(board)
     if not find:
         return True
     else:
         row, col = find
-
+    
     for num in range(1,10):
-        if valid(board, num, (row, col)):
+        if valid_placement(board, num, (row,col)):
             board[row][col] = num
 
             if solve(board):
@@ -29,34 +28,30 @@ def solve(board : list):
                 board[row][col] = 0
     return False
 
-
-def valid(board:list, num:int, pos:tuple):
+def valid_placement(board : list, num : int, pos: tuple):
     """
     Can the number be placed here?
     """
-    row_pos, col_pos = pos
-    # Check valid row
+    row_pos , col_pos = pos
+    #Check valid row
     for col in range(LEN_BOARD):
         if board[row_pos][col] == num and col_pos != col:
             return False
-
-    # Check valid column
+    
+    #Check valid column
     for row in range(LEN_BOARD):
         if board[row][col_pos] == num and row_pos != row:
             return False
-
-    # Check box
+    
+    #Check box
     start_box_x = (row_pos // 3) * 3
     start_box_y = (col_pos // 3) * 3
-    print(start_box_x)
-    print(start_box_y)
     for row in range(start_box_x, start_box_x + 3):
         for col in range(start_box_y, start_box_y + 3):
             if board[row][col] == num and (row,col) != pos:
                 return False
-
+    
     return True
-
 
 def print_board(board : list):
     """
@@ -65,16 +60,14 @@ def print_board(board : list):
     for row in range(LEN_BOARD):
         if row % 3 == 0 and row != 0:
             print("- - - - - - - - - - - - - ")
-
+    
         for col in range(LEN_BOARD):
             if col % 3 == 0 and col != 0:
                 print(" | ", end="")
-
-            if col == 8:
-                print(board[row][col])
+            if col != 8:
+                print(f"{board[row][col]} " ,end = "")
             else:
-                print(str(board[row][col]) + " ", end="")
-
+                print(board[row][col])
 
 def next_empty_space(board : list):
     """
@@ -84,8 +77,7 @@ def next_empty_space(board : list):
     for row in range(LEN_BOARD):
         for col in range(LEN_BOARD):
             if board[row][col] == 0:
-                return (row, col)
-
+                return (row,col)
     return None
 
 print_board(board)
